470560ba89a3d46adf255a8fb1a50013b82f76c9
Pixel Savvy <web.pixelwork@gmail.com>
2024-01-15 19:14:02 +0400

    commit

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,10 +1,10 @@
-import classes from "./Loader.module.css";
+import styles from "./Loader.module.css";
 
 const Loader = () => {
   return (
     <div className="fixed w-full min-h-screen z-[100000] bg-primary  left-0 top-0 flex flex-col justify-center items-center gap-10">
-      <span className={classes.loader} />
-      <span className={`${classes.text} font-tbc-bld text-5xl`}>იტვირთება</span>
+      <span className={styles.loader} />
+      <span className={`${styles.text} font-tbc-bld text-5xl`}>იტვირთება</span>
     </div>
   );
 };
